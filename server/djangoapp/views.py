518a62fd1a804c6ec4b13b2592b5d01a8c1a2315
TobyAjLot <tobyajlot@gmail.com>
2023-07-27 15:11:23 -0400

    Create Proxy Services for Cloud Functions

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
-# from .models import related models
-# from .restapis import related methods
+from .models import CarDealer, CarMake
+from .restapis import get_dealers_from_cf, get_dealer_reviews_from_cf
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
@@ -80,14 +80,25 @@ def registration_request(request):
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
-    context = {}
     if request.method == "GET":
-        return render(request, 'djangoapp/index.html', context)
+        url = "https://eu-gb.functions.appdomain.cloud/api/v1/web/84101271-33c3-4cbf-8799-440e5b09df0b/dealership-package/get-dealership"
+        # Get dealers from the URL
+        dealerships = get_dealers_from_cf(url)
+        # Concat all dealer's short name
+        dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
+        # Return a list of dealer short name
+        return HttpResponse(dealer_names)
 
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
-# def get_dealer_details(request, dealer_id):
+def get_dealer_details(request, dealer_id):
 # ...
+    if request.method == "GET":
+        url = "https://eu-gb.functions.appdomain.cloud/api/v1/web/84101271-33c3-4cbf-8799-440e5b09df0b/dealership-package/get-review"
+        dealer_id = ' '.join([review.id for review in reviews])
+        reviews = get_dealer_reviews_from_cf(url, dealer_id)
+
+        return HttpResponse(dealer_id)
 
 # Create a `add_review` view to submit a review
 # def add_review(request, dealer_id):
